951edf5b045954c462502114f2850f581d920c6c
higgs.avila <johnhigginsavila@Johns-MacBook-Pro.local>
2023-05-30 18:42:00 +0800

    fix deployment issue

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -44,9 +44,8 @@ module "blog_autoscaling" {
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -87,7 +86,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "${var.environment.name}-blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
